1409ba1d1ca38547322d46758babdf54bb6b2f93
victor.o.antonino <victor.o.antonino@accenture.com>
2016-09-14 10:18:43 -0300

    First steps with TableView

--- MyFirstApp/ProductsTableViewController.swift
+++ MyFirstApp/ProductsTableViewController.swift
@@ -9,5 +9,18 @@
 import UIKit
 
 class ProductsTableViewController: UITableViewController {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell", forIndexPath: indexPath) as UITableViewCell
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
