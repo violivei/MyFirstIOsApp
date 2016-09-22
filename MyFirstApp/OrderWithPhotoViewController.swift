7af09796e91635fdafb76ab19af2eaba87b2daf7
victor.o.antonino <victor.o.antonino@accenture.com>
2016-09-22 19:15:39 -0300

    First steps with Camera

---
+++ MyFirstApp/OrderWithPhotoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OrderWithPhotoViewController.swift
+//  MyFirstApp
+//
+//  Created by Antonino, Victor O. on 22/09/16.
+//  Copyright © 2016 Accenture. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func useCamera(sender: AnyObject) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+}
