4b0bf77f1f490ce061ac557ab51499089cb52e34
victor.o.antonino <victor.o.antonino@accenture.com>
2016-09-29 19:53:35 -0300

    image rest api

--- MyFirstApp/OrderWithPhotoViewController.swift
+++ MyFirstApp/OrderWithPhotoViewController.swift
@@ -41,6 +41,57 @@ class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDel
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func callWatson(){
+
+        let url = NSURL(string: "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=54a2ea093fbed06393dab35593dc51f785b493c5&version=2016-09-23")
+        let request = NSMutableURLRequest(url: url! as URL)
+        request.httpMethod = "POST"
+        request.setValue("multipart/form-data", forHTTPHeaderField: "content-type")
+        request.httpBody = NSData(contentsOf: photoURL!) as Data?
+        
+        let connection = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
+            if let urlData = data {
+                do {
+                    let response = try JSONSerialization.jsonObject(with: urlData, options: JSONSerialization.ReadingOptions.mutableLeaves) as! NSDictionary
+                    
+                    if let res = response["images"] {
+                        let resArr = res as! NSArray
+                        
+                        if resArr.count > 0 {
+                            let classifiersDict = resArr[0] as! NSDictionary
+                            let classifiers = classifiersDict["classifiers"] as! NSArray
+                            let classesDict = classifiers[0] as! NSDictionary
+                            let classes = classesDict["classes"]! as! NSArray
+                            let cla = classes[0] as! NSDictionary
+                            let classString = cla["class"] as! String
+                            
+                            DispatchQueue.main.async(execute: { () -> Void in
+                                NSLog(classString)
+                            })
+                        } else {
+                            DispatchQueue.main.async(execute: { () -> Void in
+                                NSLog("No results found. Please try again.")
+                            })
+                        }
+                    } else {
+                        DispatchQueue.main.async(execute: { () -> Void in
+                            NSLog("No results found. Please try again2.")
+                        })
+                    }
+                } catch let err as NSError{
+                    print(err.localizedDescription)
+                }
+            } else {
+                print(error?.localizedDescription)
+                DispatchQueue.main.async(execute: { () -> Void in
+                    NSLog(error!.localizedDescription)
+                })
+            }
+        }
+        
+        connection.resume()
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         imagePicker.dismiss(animated: true, completion: nil)
