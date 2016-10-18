5088d3a70e7ab626567e26b90f561a6869358faa
Antonino, Victor O <victor.o.antonino@accenture.com>
2016-10-18 14:46:30 -0300

    modifications

--- MyFirstApp/OrderWithPhotoViewController.swift
+++ MyFirstApp/OrderWithPhotoViewController.swift
@@ -11,7 +11,7 @@ import MobileCoreServices
 import VisualRecognitionV3
 
 class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet var imageView: UIImageView!
     var imagePicker: UIImagePickerController!
     var apiKey: String = "54a2ea093fbed06393dab35593dc51f785b493c5"
@@ -26,7 +26,7 @@ class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDel
     @IBAction func sendPicture(_: AnyObject) {
         callWatson()
     }
-
+    
     @IBAction func useCamera(_: AnyObject) {
         imagePicker =  UIImagePickerController()
         imagePicker.delegate = self
@@ -40,64 +40,181 @@ class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDel
         return "Boundary-\(NSUUID().UUIDString)"
     }
     
-    func callWatson(){
+    // this is a very verbose version of that function
+    // you can shorten it, but i left it as-is for clarity
+    // and as an example
+    func photoDataToFormData(data:NSData,boundary:String,fileName:String) -> NSData {
+        var fullData = NSMutableData()
         
-        let failure = { (error: NSError) in print(error) }
-        NSLog("%@", "SENDING: \(imageFileURL().path!)")
-        visualRecognition.classify(imageFileURL(), failure: failure) { classifiedImages in
-            print(classifiedImages)
-            NSLog("%@", "Result: \(classifiedImages)")
-        }
+        // 1 - Boundary should start with --
+        let lineOne = "--" + boundary + "\r\n"
+        fullData.appendData(lineOne.dataUsingEncoding(
+            NSUTF8StringEncoding,
+            allowLossyConversion: false)!)
         
-        /*let url = NSURL(string: "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=54a2ea093fbed06393dab35593dc51f785b493c5&version=2016-05-20")
-        let request = NSMutableURLRequest(URL: url! as NSURL)
-        request.HTTPMethod = "POST"
+        // 2
+        let lineTwo = "Content-Disposition: form-data; name=\"images_file\"; filename=\"" + fileName + "\"\r\n"
+        NSLog(lineTwo)
+        fullData.appendData(lineTwo.dataUsingEncoding(
+            NSUTF8StringEncoding,
+            allowLossyConversion: false)!)
+        
+        // 3
+        let lineThree = "Content-Type: image/jpg\r\n\r\n"
+        fullData.appendData(lineThree.dataUsingEncoding(
+            NSUTF8StringEncoding,
+            allowLossyConversion: false)!)
+        
+        // 4
+        fullData.appendData(data)
+        
+        // 5
+        let lineFive = "\r\n"
+        fullData.appendData(lineFive.dataUsingEncoding(
+            NSUTF8StringEncoding,
+            allowLossyConversion: false)!)
+        
+        // 6 - The end. Notice -- at the start and at the end
+        let lineSix = "--" + boundary + "--\r\n"
+        fullData.appendData(lineSix.dataUsingEncoding(
+            NSUTF8StringEncoding,
+            allowLossyConversion: false)!)
+        
+        return fullData
+    }
+    
+    func sendFile(
+        urlPath:String,
+        fileName:String,
+        data:NSData){
+        
+        var url: NSURL = NSURL(string: urlPath)!
+        var request1: NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        
+        request1.HTTPMethod = "POST"
         
         let boundary = generateBoundaryString()
+        let fullData = photoDataToFormData(data,boundary:boundary,fileName:fileName)
         
-        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
-        let body = NSMutableData()
+        request1.setValue("multipart/form-data; boundary=" + boundary,
+                          forHTTPHeaderField: "Content-Type")
         
-        //let fname = "temp.jpg"
-        //let mimetype = "image/jpg"
+        // REQUIRED!
+        request1.setValue(String(fullData.length), forHTTPHeaderField: "Content-Length")
         
-        let imageData : UIImage?
-        var image_data : NSData?
+        request1.HTTPBody = fullData
+        request1.HTTPShouldHandleCookies = false
         
-        if let data = NSData(contentsOfURL: photoURL!) {
-            imageData = UIImage(data: data as NSData)
-            image_data = UIImageJPEGRepresentation(imageData!, 0.2)
+        let queue:NSOperationQueue = NSOperationQueue()
+        
+        do {
+            let returnData = try NSURLConnection.sendSynchronousRequest(request1, returningResponse: nil)
+            let returnString = NSString(data: returnData, encoding: NSUTF8StringEncoding)
+            print("returnString = \(returnString!)")
         }
-        
-        body.appendData("--\(boundary)\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        //body.append("Content-Disposition:form-data; name=\"test\"\r\n\r\n".data(using: String.Encoding.utf8)!)
-        //body.append("hi\r\n".data(using: String.Encoding.utf8)!)
-        
-        //body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
-        body.appendData("Content-Disposition:form-data; name=\"images_file\"; filename=\"\"\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        body.appendData(image_data!.base64EncodedDataWithOptions(NSDataBase64EncodingOptions()))
-        body.appendData("Content-Type:\n\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        //body.append("\n\n".data(using: String.Encoding.utf8)!)
-        
-        body.appendData("--\(boundary)--\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
-        request.HTTPBody = body as NSData
-        
-        //request.httpBody = image_data
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request)
-            {(data, response, error) in
-            
-            guard let _:NSData = data, let _:NSURLResponse = response where error == nil else {
-                print("error")
-                return
-            }
-            
-            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            
-            print(dataString)            
+        catch let  error as NSError {
+            print(error.description)
         }
-        task.resume()*/
+        //NSURLConnection.sendAsynchronousRequest(
+        //request1,
+        //queue: queue,
+        //completionHandler:completionHandler)
+    }
+    
+    func callWatson(){
+        
+        let url = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=54a2ea093fbed06393dab35593dc51f785b493c5&version=2016-05-20"
+        //let img = UIImage(contentsOfFile: fullPath)
+        let data: NSData = UIImageJPEGRepresentation(imageView.image!, 0.2)!
+        
+        sendFile(url,
+                 fileName:"one.jpg",
+                 data:data)
+        /*let failure = { (error: NSError) in print(error) }
+         NSLog("%@", "SENDING: \(imageFileURL().path!)")
+         visualRecognition.classify(imageFileURL(), failure: failure) { classifiedImages in
+         print(classifiedImages)
+         NSLog("%@", "Result: \(classifiedImages)")
+         }*/
+        
+        /*let str = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=54a2ea093fbed06393dab35593dc51f785b493c5&version=2016-05-20"
+         let request = NSMutableURLRequest(URL: NSURL(string:str)!)
+         request.HTTPMethod = "POST"
+         
+         let boundary = NSString(format: "---------------------------14737809831466499882746641449")
+         
+         let contentType = NSString(format: "multipart/form-data; boundary=%@",boundary)
+         request.addValue(contentType as String, forHTTPHeaderField: "Content-Type")
+         
+         let body = NSMutableData()
+         let imageData = UIImageJPEGRepresentation(imageView.image!, 0.2)
+         // append image data to body
+         body.appendData(NSString(format: "\r\n--%@\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding)!)
+         body.appendData(NSString(format:"Content-Disposition: form-data; name=\"%@images_file\"; filename=\"img.jpg\"\\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+         body.appendData(NSString(format: "Content-Type: application/octet-stream\r\n\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+         body.appendData(imageData!)
+         body.appendData(NSString(format: "\r\n--%@\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding)!)
+         
+         request.HTTPBody = body
+         
+         do {
+         let returnData = try NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+         let returnString = NSString(data: returnData, encoding: NSUTF8StringEncoding)
+         print("returnString = \(returnString!)")
+         }
+         catch let  error as NSError {
+         print(error.description)
+         }*/
+        
+        /*let url = NSURL(string: "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=54a2ea093fbed06393dab35593dc51f785b493c5&version=2016-05-20")
+         let request = NSMutableURLRequest(URL: url! as NSURL)
+         request.HTTPMethod = "POST"
+         
+         let boundary = generateBoundaryString()
+         
+         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+         
+         let body = NSMutableData()
+         
+         //let fname = "temp.jpg"
+         //let mimetype = "image/jpg"
+         
+         let imageData : UIImage?
+         var image_data : NSData?
+         
+         if let data = NSData(contentsOfURL: photoURL!) {
+         imageData = UIImage(data: data as NSData)
+         image_data = UIImageJPEGRepresentation(imageData!, 0.2)
+         }
+         
+         body.appendData("--\(boundary)\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
+         //body.append("Content-Disposition:form-data; name=\"test\"\r\n\r\n".data(using: String.Encoding.utf8)!)
+         //body.append("hi\r\n".data(using: String.Encoding.utf8)!)
+         
+         //body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
+         body.appendData("Content-Disposition:form-data; name=\"images_file\"; filename=\"\"\n".dataUsingEncoding(NSUTF8StringEncoding)!)
+         body.appendData(image_data!.base64EncodedDataWithOptions(NSDataBase64EncodingOptions()))
+         body.appendData("Content-Type:\n\n".dataUsingEncoding(NSUTF8StringEncoding)!)
+         //body.append("\n\n".data(using: String.Encoding.utf8)!)
+         
+         body.appendData("--\(boundary)--\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
+         request.HTTPBody = body as NSData
+         
+         //request.httpBody = image_data
+         
+         let task = NSURLSession.sharedSession().dataTaskWithRequest(request)
+         {(data, response, error) in
+         
+         guard let _:NSData = data, let _:NSURLResponse = response where error == nil else {
+         print("error")
+         return
+         }
+         
+         let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+         
+         print(dataString)
+         }
+         task.resume()*/
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
@@ -107,12 +224,12 @@ class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDel
         //let jpegImage = UIImageJPEGRepresentation(imageView.image!, 0.2)
         //jpegImage!.writeToFile(imageFileURL().path!, atomically: true)
         /*jpegImage!.writeToFile(imageFileURL().path!, atomically: true)
-        if let data = UIImageJPEGRepresentation(imageView.image!, 1.0) {
-            let filename = getDocumentsDirectory().URLByAppendingPathComponent("temp.jpg")
-            data.writeToFile((filename?.absoluteString)!, atomically: true)
-            NSLog("%@", "Loading page with URL: \(filename)")
-            photoURL = filename
-        }*/
+         if let data = UIImageJPEGRepresentation(imageView.image!, 1.0) {
+         let filename = getDocumentsDirectory().URLByAppendingPathComponent("temp.jpg")
+         data.writeToFile((filename?.absoluteString)!, atomically: true)
+         NSLog("%@", "Loading page with URL: \(filename)")
+         photoURL = filename
+         }*/
         imagePicker.dismissViewControllerAnimated(true, completion: nil)
         
     }
@@ -162,8 +279,8 @@ class OrderWithPhotoViewController: UIViewController, UIImagePickerControllerDel
     }
     
     func getDocumentsDirectory() -> NSURL {
-       let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-
+        let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        
         let documentsDirectory = paths[0]
         return documentsDirectory
     }
